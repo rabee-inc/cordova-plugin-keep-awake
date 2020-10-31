7bd380b5659e77f54fecd99ddb25f6b3cc589283
snuffy <snuffy.tokyo@gmail.com>
2020-10-31 13:27:36 +0900

    feat: move from snuffy repository

---
+++ src/ios/CDVKeepAwake.swift
@@ -0,0 +1,18 @@
+
+
+@objc(CDVKeepAwake) class CDVKeepAwake : CDVPlugin {
+  override func pluginInitialize() {};
+
+  @objc func start(_ command: CDVInvokedUrlCommand) {
+    UIApplication.shared.isIdleTimerDisabled = true
+    let result = CDVPluginResult(status: CDVCommandStatus_OK)
+    commandDelegate.send(result, callbackId: command.callbackId)
+    
+  }
+
+  @objc func stop(_ command: CDVInvokedUrlCommand) {
+    UIApplication.shared.isIdleTimerDisabled = false
+    let result = CDVPluginResult(status: CDVCommandStatus_OK)
+    commandDelegate.send(result, callbackId: command.callbackId)
+  }
+}
